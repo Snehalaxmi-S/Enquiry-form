709da48f3d10b522f7128fe6836724fb1d6d2f3d
Snehalaxmi-S <snehalaxmi2104@gmail.com>
2023-06-20 16:37:14 +0530

    Updated fields

--- my-app/src/contact/Contact.jsx
+++ my-app/src/contact/Contact.jsx
@@ -11,15 +11,17 @@ export default function Contact() {
             <label>Email<span className="star">*</span></label>
             <input type="text" className="contactInput" placeholder="Enter your email..." required />
             <label>Phone Number<span className="star">*</span></label>
-            <input type="number" className="contactInput" placeholder="Enter your phone number..."required />
+            <input type="integer" className="contactInput" placeholder="Enter your phone number..."required />
+            <label>Bill Number<span className="star">*</span></label>
+            <input type="integer" className="contactInput" placeholder="Enter your bill number..."required />
             <label>Subject<span className="star">*</span></label>
             <input type="text" className="contactInput" placeholder="Enter the subject of the enquiry..."required />
-            <label>Message<span className="star">*</span></label>
-            <input type="textarea" className="contactInput textarea" placeholder="Enter your message..."required />
+            <label>Customer Enquiry<span className="star">*</span></label>
+            <input type="textarea" className="contactInput textarea" placeholder="Enter the content of enquiry..."required />
             <label>Add your file</label>
             <input type="file" className="contactInput"/>
 
-            <button className="contactButton">Submit</button>
+            <center><button className="contactButton">Submit</button></center>
         </form>
     </div>
   )
